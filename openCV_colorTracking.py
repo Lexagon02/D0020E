d78d6f3eb339dca0d8f2210d172102f1b6385956
Lexagon02 <leo.r.bergstrom@gmail.com>
2023-12-13 13:07:34 +0100

    multiple color tracking

--- openCV_colorTracking.py
+++ openCV_colorTracking.py
@@ -30,7 +30,7 @@ def onTrack6(val):
 
 
 width=1280
-height=720
+height=680
 
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -38,32 +38,40 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 60)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
+#cv2.namedWindow('myTracker')
+#cv2.moveWindow('myTracker' ,width,0)
 
 
-cv2.namedWindow('myTracker')
-cv2.moveWindow('myTracker' ,width,0)
+#    hueLow=10
+#    hueHigh=20
+#    satLow=10
+#    satHigh=250
+#    valLow=10
+#    valHigh=250
+#    cv2.createTrackbar('Hue Low','myTracker',10,179,onTrack1)
+#    cv2.createTrackbar('Hue High','myTracker',20,179,onTrack2)
+#    cv2.createTrackbar('Sat Low','myTracker',10,255,onTrack3)
+#    cv2.createTrackbar('Sat High','myTracker',250,255,onTrack4)
+#    cv2.createTrackbar('Val Low','myTracker',10,255,onTrack5)
+#    cv2.createTrackbar('Val High','myTracker',250,255,onTrack6)
 
-hueLow=10
-hueHigh=20
-satLow=10
-satHigh=250
-valLow=10
-valHigh=250
+lower_blue = np.array([110,50,50])
+upper_blue = np.array([130,255,255])
 
-cv2.createTrackbar('Hue Low','myTracker',10,179,onTrack1)
-cv2.createTrackbar('Hue High','myTracker',20,179,onTrack2)
-cv2.createTrackbar('Sat Low','myTracker',10,255,onTrack3)
-cv2.createTrackbar('Sat High','myTracker',250,255,onTrack4)
-cv2.createTrackbar('Val Low','myTracker',10,255,onTrack5)
-cv2.createTrackbar('Val High','myTracker',250,255,onTrack6)
+lower_red = np.array([140,50,50])
+upper_red = np.array([179,255,255])
 
 
 while True:
     ignore,  frame = cam.read()
     frameHSV=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    lowerBound=np.array([hueLow,satLow,valLow])
-    upperBound=np.array([hueHigh,satHigh,valHigh])
-    myMask=cv2.inRange(frameHSV,lowerBound,upperBound)
+
+    #lowerBound=np.array([hueLow,satLow,valLow])
+    #upperBound=np.array([hueHigh,satHigh,valHigh])
+
+    #myMask=cv2.inRange(frameHSV,lowerBound,upperBound)
+    myMaskBlue=cv2.inRange(frameHSV,lower_blue,upper_blue)
+    myMaskRed=cv2.inRange(frameHSV,lower_red,upper_red)
     #myMask=cv2.bitwise_not(myMask)
     myObject=cv2.bitwise_and(frame,frame,mask=myMask)
     myObjectSmall=cv2.resize(myObject,(int(width/2),int(height/2)))
