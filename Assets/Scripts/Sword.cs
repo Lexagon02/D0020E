75bffae57aaa97691eb5f1fa58675ec1bd6964de
Stromo01 <oscar.axelstrom@hotmail.se>
2023-12-28 13:28:51 +0100

    The whole project

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -7,24 +7,41 @@ using UnityEditor.Scripting.Python;
 using UnityEditor;
 using static System.Net.Mime.MediaTypeNames;
 
+
+
 public class swordObject : MonoBehaviour
 {
+    public int redCoord=0;
     int i=0;
-
+    public GameObject[] left;
+    //redCoord = PythonSettings.createInstance(int);
     private void Start()
     {
+        left = new GameObject[1];
     }
 
     private void Update()
     {
         if (i > 100) { 
-            [MenuItem("Python Scripts/get")]
+/*             [MenuItem("Python Scripts/get")]
             static void get()
             {
                 PythonRunner.RunFile($"C:/Users/oscar/My project/Assets/get.py");
             }
-            get();
+            get(); */
+            
+
+            [MenuItem("Python Scripts/setCoord")]
+            static void setCoord()
+                {
+                 PythonRunner.RunString(@"
+                    import UnityEngine;
+                    UnityEngine.left.redCoord=3
+                ");
+                }
+            setCoord();
             i = 0;
+
         }
         i = i + 1;
 
